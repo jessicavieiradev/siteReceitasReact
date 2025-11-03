aafd15579b0a4d26af3c96b8512c35e2873ffad2
“jessicavieiradev” <misspandaicebear@gmail.com>
2025-11-03 14:53:41 -0300

    base alterada

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,13 @@
-import { createRoot } from "react-dom/client";
-import "./index.css";
-import App from "./App.tsx";
-import { BrowserRouter} from "react-router-dom";
-import DataProviderRecipes from "./components/DataProviderRecipes/DataProviderRecipes.tsx";
+  import { createRoot } from "react-dom/client";
+  import "./index.css";
+  import App from "./App.tsx";
+  import { BrowserRouter} from "react-router-dom";
+  import DataProviderRecipes from "./components/DataProviderRecipes/DataProviderRecipes.tsx";
 
-createRoot(document.getElementById("root")!).render(
-  <DataProviderRecipes>
-    <BrowserRouter basename={import.meta.env.BASE_URL}>
-      <App />
-    </BrowserRouter>
-  </DataProviderRecipes>
-);
+  createRoot(document.getElementById("root")!).render(
+    <DataProviderRecipes>
+      <BrowserRouter basename={import.meta.env.BASE_URL}>
+        <App />
+      </BrowserRouter>
+    </DataProviderRecipes>
+  );
